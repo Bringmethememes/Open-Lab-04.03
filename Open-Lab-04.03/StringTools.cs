f24a3c1144485484dff60a8e4684d64a3873bc5c
PC <PC@ANDREJKOVPOITA>
2020-10-23 22:05:40 +0200

    Blyat ale nie viktorov

--- Open-Lab-04.03/StringTools.cs
+++ Open-Lab-04.03/StringTools.cs
@@ -6,7 +6,43 @@ namespace Open_Lab_04._03
     {
         public string RemoveFirstLast(string original)
         {
-            throw new NotImplementedException();
+            if (original.Length < 2)
+            {
+                return original;
+            }
+            else
+            {
+                char[] bruh = new char[original.Length - 1];
+                for (int i = 0; i < original.Length- 1; i++)
+                {
+                    bruh[i] = original[i];
+                }
+                Array.Reverse(bruh);
+                char[] bruh2 = new char[bruh.Length - 1];
+                for (int i = 0; i < bruh.Length - 1; i++)
+                {
+                    bruh2[i] = bruh[i];
+                }
+                Array.Reverse(bruh2);
+                string blyat = string.Join("", bruh2);
+                return blyat;
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+
+
         }
     }
 }
